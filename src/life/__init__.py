fac8b3e2af1b1fb3991c5cc1d5be5da84ee7dec2
algoq <algoq2039@proton.me>
2026-01-07 15:16:05 +0100

    Add src/life/__init__.py

---
+++ src/life/__init__.py
@@ -0,0 +1,20 @@
+"""
+Life: Conceptual Intelligence System
+
+A simple, intuitive intelligence system that learns concepts, ideologies,
+and philosophies through human-like reasoning.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Algoq"
+__license__ = "Apache 2.0"
+
+from life.concept_graph import ConceptGraph
+from life.metacognition import MetacognitionSpace
+from life.ideology import IdeologyFramework
+
+__all__ = [
+    "ConceptGraph",
+    "MetacognitionSpace",
+    "IdeologyFramework",
+]
